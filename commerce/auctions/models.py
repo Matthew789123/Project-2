0d8a93ac72ec66877a66b5613021494d4e760a83
Matthew789123 <Matthew789123@gmail.com>
2020-10-08 16:02:59 +0200

    Added Listing, Bid, Category

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bid(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=1000)
+    bid = models.ForeignKey(Bid, on_delete=models.PROTECT)
+    image = models.CharField(max_length=64, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
